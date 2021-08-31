4911ed5f26a71e18f4cf5a6f7bcafdb145941bf8
Anirudh Sarma <asarma31@gatech.edu>
2021-08-31 00:56:18 -0400

    fix bug in conf_scripts

--- conf/scripts/copy_to_vms.sh
+++ conf/scripts/copy_to_vms.sh
@@ -11,10 +11,16 @@ num_vms=$1
 # Please specify template file
 template=$2
 
+cur_dir=$(basename $PWD)
+if [ ${cur_dir} == "scripts" ]; then
+  echo "Please run script from Parent"
+  exit 1;
+fi
+
 mkdir -p gen_conf
 gen_path=$(realpath gen_conf)
 
-python3 conf_generator.py -n $num_vms -t $template -p ${gen_path}
+python3 scripts/conf_generator.py -n $num_vms -t $template -p ${gen_path}
 
 for ((i=0; i<${num_vms}; i++)); do
   rsync -avz ${gen_path}/edgeConfig$i.json cv$((i+1)):~/clairvoyant2/conf/
