6e7636a17318b7912ac81f91de8577ca81f24278
Manasvini <msethuraman3@gatech.edu>
2022-01-04 16:47:37 +0000

    update cloud svc to use new meta repo

--- run_scripts/start_cloud_services.sh
+++ run_scripts/start_cloud_services.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 num_nodes=$1
+conf=$2
 cd
-cd clairvoyantedge-comm
+cd clairvoyantedge-metadata
 
 curdir=$(pwd)
-#pkill redis
-#rm -rf cluster/*
+pkill redis
+rm -rf cluster/*
 bash scripts/start_redis_cluster.sh 3
-sleep 5
-cd evaluation/uploader_scripts
+sleep 15
+
 echo "adding nodes"
-cd ~/clairvoyant-decentralized
-python3 scripts/add_nodes.py -n $num_nodes -p "192.168.160." -s 22 -i 0 -f enode_positions/monaco_traffic_lights.csv
+cd ~/clairvoyant2
+
+python3 eval/add_nodes.py -n $num_nodes -p "192.168.160." -s 22 -i 0 -f "eval/enode_positions/monaco_traffic_lights.csv"
 
 cd $curdir
 pkill clairvoyant
 bash scripts/start_meta_svr.sh > ./logs/meta_svr.out 2>&1 &
 
 cd metadata/scripts
-bash video_creator.sh ../../build/metadata/scripts/data-uploader 210 bbb_1080p.csv
-
+#bash video_creator.sh ../../build/metadata/scripts/data-uploader 210 bbb_1080p.csv
+#
 pkill python3
 cd ~/clairvoyant2
 mkdir -p logs
-
+#
 
 #python3 src/CVCloudServer.py -a 0.0.0.0:50059 -c conf/${num_nodes}node_cloudConfig.json  > logs/cloudserver.log 2>&1 &
-python3 src/cloud_runner.py -a 0.0.0.0:60050 -c conf/10node_monacoCloudConfig.json  > logs/cloud.log 2>&1 &
+python3 src/cloud_runner.py -a 0.0.0.0:60050 -c $conf  > logs/cloud.log 2>&1 &
 #python3 scripts/start_client_runner.sh
