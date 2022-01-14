54a2069fda7528d1317f53a690419a918e75f63b
Manasvini <msethuraman3@gatech.edu>
2022-01-14 08:41:34 -0500

    add segment file as param

--- run_scripts/run_eval.py
+++ run_scripts/run_eval.py
@@ -14,10 +14,11 @@ def start_cloud(conf):
     numEdgeNodes = conf['numEdgeNodes']
     cloudConfig = conf['cloudConfig']
     edgePos = conf['edgePositions']
-    numVideos = conf["numUsers"] * 2
+    numVideos = conf["numUsers"] * 2 + 1
+    segmentFile = conf['segmentFile']
     result = subprocess.run(['ssh', conf['cloudServerName'], \
                             'cd clairvoyant2 && bash run_scripts/start_cloud_services.sh ' +\
-                             str(numEdgeNodes) + ' ' + cloudConfig + ' ' + edgePos + ' ' + str(numVideos)], \
+                             str(numEdgeNodes) + ' ' + cloudConfig + ' ' + edgePos + ' ' + str(numVideos) + ' ' + segmentFile], \
                              shell=False, \
                              check=False)
     print('cloud success=', result.returncode) 
@@ -33,11 +34,11 @@ def start_edge(conf):
 
 def start_clients(conf):
     numClients = conf["numUsers"]
-    numVideos = conf['numUsers'] * 2
+    numVideos = conf['numUsers'] * 2 
     clientTrajectories = conf['trajectoriesDir']
     edgeInfo = conf['edgeInfoFile']
     outputFile = conf['outputDir'] + "/" + str(numClients) + 'users' + str(conf['numEdgeNodes']) + 'nodes' + str(round(time.time() * 1000)) + '.csv'
-
+    segmentFile = conf['segmentFile']
     cloudServer = conf['cloudServerName']
     result = subprocess.run(['ssh', cloudServer, ' export PATH=$PATH:/usr/local/go/bin && cd clairvoyant2/src/clientrunner_go && go run . ' +\
                             '-log_dir=\".\"'+\
@@ -45,7 +46,8 @@ def start_clients(conf):
                             ' -i ' + str(numVideos) + \
                             ' -n ' + str(numClients) + \
                             ' -o ' + outputFile +\
-                            ' -e ' + edgeInfo], \
+                            ' -e ' + edgeInfo + \
+                            ' -f ' + segmentFile], \
                             shell=False,\
                             check=False)
     print('client success=', result.returncode)
