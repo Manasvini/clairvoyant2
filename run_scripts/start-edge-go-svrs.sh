0ce735a587c2a2ee294a3a065cc9a8a1574fa4f9
Manasvini <msethuraman3@gatech.edu>
2022-02-13 14:15:13 -0500

    clean up conf dir

--- run_scripts/start-edge-go-svrs.sh
+++ run_scripts/start-edge-go-svrs.sh
@@ -11,7 +11,7 @@ bash run_scripts/kill-edge-go-svrs.sh $num_edge $machine_prefix
 for ((i=1;i<=num_edge;i++)); do
   confid=$(($i-1))
   nodeid=$i
-  scp conf/gen_conf/edgeConfig$confid.json cvuser@${machine_prefix}${nodeid}:~/clairvoyant2/conf/edgeConfig.json
+  scp conf/edge/edgeConfig$confid.json cvuser@${machine_prefix}${nodeid}:~/clairvoyant2/conf/edgeConfig.json
   ssh ${machine_prefix}$i bash <<EOF
 cd clairvoyant2/
 mkdir -p logs
