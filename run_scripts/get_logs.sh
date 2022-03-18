8591229124ef75f48063d683a54843b844380716
Adwait Bauskar <adwait.bauskar@gatech.edu>
2022-03-18 14:28:25 -0400

    removed python edge logs from get logs

--- run_scripts/get_logs.sh
+++ run_scripts/get_logs.sh
@@ -2,17 +2,6 @@ rm -rf all_logs
 mkdir all_logs
 cp out.txt ./all_logs/out.txt
 
-scp cvuser@cv1:~/clairvoyant2/logs/edge.out ./all_logs/edge1.out
-scp cvuser@cv2:~/clairvoyant2/logs/edge.out ./all_logs/edge2.out
-scp cvuser@cv3:~/clairvoyant2/logs/edge.out ./all_logs/edge3.out
-scp cvuser@cv4:~/clairvoyant2/logs/edge.out ./all_logs/edge4.out
-scp cvuser@cv5:~/clairvoyant2/logs/edge.out ./all_logs/edge5.out
-scp cvuser@cv6:~/clairvoyant2/logs/edge.out ./all_logs/edge6.out
-scp cvuser@cv7:~/clairvoyant2/logs/edge.out ./all_logs/edge7.out
-scp cvuser@cv8:~/clairvoyant2/logs/edge.out ./all_logs/edge8.out
-scp cvuser@cv9:~/clairvoyant2/logs/edge.out ./all_logs/edge9.out
-scp cvuser@cv10:~/clairvoyant2/logs/edge.out ./all_logs/edge10.out
-
 scp cvuser@cv1:~/clairvoyant2/src/edge_go/edge_go.INFO ./all_logs/edge_go1.INFO
 scp cvuser@cv2:~/clairvoyant2/src/edge_go/edge_go.INFO ./all_logs/edge_go2.INFO
 scp cvuser@cv3:~/clairvoyant2/src/edge_go/edge_go.INFO ./all_logs/edge_go3.INFO
