f9c959be0a0916651d8172d8c5fef1c2ea6bc9b0
Adwait Bauskar <adwait.bauskar@gatech.edu>
2022-03-18 09:56:33 -0400

    Extended the get_logs file to include out for edge

--- run_scripts/get_logs.sh
+++ run_scripts/get_logs.sh
@@ -24,6 +24,18 @@ scp cvuser@cv8:~/clairvoyant2/src/edge_go/edge_go.INFO ./all_logs/edge_go8.INFO
 scp cvuser@cv9:~/clairvoyant2/src/edge_go/edge_go.INFO ./all_logs/edge_go9.INFO
 scp cvuser@cv10:~/clairvoyant2/src/edge_go/edge_go.INFO ./all_logs/edge_go10.INFO
 
+
+scp cvuser@cv1:~/clairvoyant2/logs.edge.out ./all_logs/edge_go1.out
+scp cvuser@cv2:~/clairvoyant2/logs.edge.out ./all_logs/edge_go2.out
+scp cvuser@cv3:~/clairvoyant2/logs.edge.out ./all_logs/edge_go3.out
+scp cvuser@cv4:~/clairvoyant2/logs.edge.out ./all_logs/edge_go4.out
+scp cvuser@cv5:~/clairvoyant2/logs.edge.out ./all_logs/edge_go5.out
+scp cvuser@cv6:~/clairvoyant2/logs.edge.out ./all_logs/edge_go6.out
+scp cvuser@cv7:~/clairvoyant2/logs.edge.out ./all_logs/edge_go7.out
+scp cvuser@cv8:~/clairvoyant2/logs.edge.out ./all_logs/edge_go8.out
+scp cvuser@cv9:~/clairvoyant2/logs.edge.out ./all_logs/edge_go9.out
+scp cvuser@cv10:~/clairvoyant2/logs.edge.out ./all_logs/edge_go10.out
+
 scp cvuser@cv0:~/clairvoyant2/logs/cloud.log ./all_logs/cloud.log
 scp cvuser@cv0:~/clairvoyant2/logs/clock.log ./all_logs/clock.log
 
