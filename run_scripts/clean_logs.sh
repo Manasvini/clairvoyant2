57bc146f7101bef30fe0afd996f53f234396cf90
Adwait Bauskar <adwait.bauskar@gatech.edu>
2022-03-23 15:22:05 -0400

    Added scripts to clean and retrieve logs

---
+++ run_scripts/clean_logs.sh
@@ -0,0 +1,13 @@
+echo "Deleting logs for cloud cv0"
+ssh cv0 rm /home/cvuser/clairvoyant2/src/clientrunner_go/clientrunner_go.INFO
+ssh cv0 rm /home/cvuser/clairvoyant2/src/clientrunner_go/clientrunner_go.WARNING
+ssh cv0 rm /home/cvuser/clairvoyant2/src/clientrunner_go/clientrunner_go.ERROR
+
+for i in {1..10};
+do
+    echo "Deleting logs for edge cv$i"
+    ssh cv$i rm /home/cvuser/clairvoyant2/src/edge_go/edge_go.INFO
+    ssh cv$i rm /home/cvuser/clairvoyant2/src/edge_go/edge_go.WARNING
+    ssh cv$i rm /home/cvuser/clairvoyant2/src/edge_go/edge_go.ERROR
+    ssh cv$i rm /home/cvuser/clairvoyant2/src/edge_go/edge_go.FATAL
+done
