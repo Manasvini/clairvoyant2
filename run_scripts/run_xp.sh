f29fbb3955fb7ff13a16f8eb26ce84ff0a975fc9
Adwait Bauskar <adwait.bauskar@gatech.edu>
2022-04-08 10:09:36 -0400

    Fixup on rsync number of vms

--- run_scripts/run_xp.sh
+++ run_scripts/run_xp.sh
@@ -9,7 +9,7 @@ echo "deleted logs"
 
 cd ~/clairvoyant2
 bash run_scripts/clean_logs.sh
-bash run_scripts/sync_with_vm.sh 11 cv
+bash run_scripts/sync_with_vm.sh 10 cv
 python3 run_scripts/run_eval.py $1 ../results_dir >> ../out.txt 2>> ../out_err.txt
 
 echo "getting logs"
