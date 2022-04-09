14d6676c8ef43ac2b9f4e3d15c3469753b52e99b
Adwait Bauskar <adwait.bauskar@gatech.edu>
2022-04-08 22:34:52 -0400

    Updated gen_protos script for python

--- run_scripts/gen_protos.sh
+++ run_scripts/gen_protos.sh
@@ -2,4 +2,4 @@ protoc --go_out=../src/edge_go/contentserver/ --go_opt=paths=source_relative \
        --go-grpc_out=../src/edge_go/contentserver/ --go-grpc_opt=paths=source_relative \
        -I ~/clairvoyant2/protos/ contentserver.proto
 
-protoc --python_out=../src/genprotos clairvoyant.proto
+python3 -m grpc_tools.protoc -I ./ --python_out=../src/genprotos --grpc_python_out=../src/genprotos clairvoyant.proto
